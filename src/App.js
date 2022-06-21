537feafec3e278eb157c01b82859068ef637af91
Riccardo Gulin <riccardo@nucleode.com>
2022-06-21 10:25:51 +0200

    Adds Bootstrap & Adds Navbar

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css" // <-- DO NOT FORGET THIS!
+import CustomNavbar from "./components/CustomNavbar"
+
+function App() {
+  return <CustomNavbar title="Epic Restaurant" />
+}
+
+export default App
